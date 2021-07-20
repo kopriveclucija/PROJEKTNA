634e2a4c3ab5dbd4042227956aae2dc8de0348cc
Lucija Koprivec <vas.email@email.com>
2021-07-20 10:56:23 +0200

    popravljanje primerov nalog

--- osnovno.py
+++ osnovno.py
@@ -3,35 +3,33 @@ import json
 
 class Model:
     def __init__(self):
-        self.naloga = Naloga('Edini spisek')
-    
-class Naloga:
-    def __init__(self, ime):
-        self.ime = ime
         self.naloge = []
-    #self.besedilo = besedilo
-    #self.resitev = resitev
-    #self.resena = False
 
-    def dodaj_nalogo(self, naloga):
-        self.naloge.append(naloga) 
+    def dodaj_novo_nalogo(self, naloga):
+        self.naloge.append(naloga)
     #doda nalogo v __init__
-
-    def stevilo_resenih_nalog(self):
-        stevilo = 0
-        for naloga in self.naloge:
-            if naloga.resena_naloga():
-                stevilo += 1
-        return stevilo
-    #koliko nalog je resenih
-
-    def katere_naloge_so_resene(self):
-        sez = []
-        for naloga in self.naloge:
-            if naloga.resena_naloga():
-                sez.append(naloga)
-        return sez
-    #katere naloge so resene seznam
+    
+#class Naloga:
+#    def __init__(self, ime):
+#        self.ime = ime
+#        self.naloge = []
+#    
+#
+#    def stevilo_resenih_nalog(self):
+#        stevilo = 0
+#        for naloga in self.naloge:
+#            if naloga.resena_naloga():
+#                stevilo += 1
+#        return stevilo
+#    #koliko nalog je resenih
+#
+#    def katere_naloge_so_resene(self):
+#        sez = []
+#        for naloga in self.naloge:
+#            if naloga.resena_naloga():
+#                sez.append(naloga)
+#        return sez
+#    #katere naloge so resene (seznam)
 
 class Posameznanaloga:
     def __init__(self, ime, besedilo, resitev):
@@ -45,3 +43,20 @@ class Posameznanaloga:
 
     def resena_naloga(self):
         return self.resena
+
+    def stevilo_resenih_nalog(self):
+        stevilo = 0
+        for naloga in self.naloge:
+            if naloga.resena_naloga():
+                stevilo += 1
+        return stevilo
+    #koliko nalog je resenih 
+
+    def katere_naloge_so_resene(self):
+        sez = []
+        for naloga in self.naloge:
+            if naloga.resena_naloga():
+                sez.append(naloga)
+        return sez
+    #katere naloge so resene (seznam)
+
