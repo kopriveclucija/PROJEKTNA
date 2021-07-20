4b5bfbebd3c3d7e94ae01ec4e1f9e35ab77e4504
Lucija Koprivec <vas.email@email.com>
2021-07-20 10:08:01 +0200

    okvirno osnovna datoteka

--- osnovno.py
+++ osnovno.py
@@ -1,16 +1,47 @@
 from datetime import date
+import json
 
 class Model:
     def __init__(self):
-        self.spisek = Zavihki('Edini spisek')
-
-class Zavihki:
+        self.naloga = Naloga('Edini spisek')
+    
+class Naloga:
     def __init__(self, ime):
         self.ime = ime
         self.naloge = []
-    
-    def stevilo_opravljenih(self):
-        stevilo = None 
+    #self.besedilo = besedilo
+    #self.resitev = resitev
+    #self.resena = False
 
-#class Dolocena_naloga:
-#    def __init__(self, ime, besedilo)
+    def dodaj_nalogo(self, naloga):
+        self.naloge.append(naloga) 
+    #doda nalogo v __init__
+
+    def stevilo_resenih_nalog(self):
+        stevilo = 0
+        for naloga in self.naloge:
+            if naloga.resena_naloga():
+                stevilo += 1
+        return stevilo
+    #koliko nalog je resenih
+
+    def katere_naloge_so_resene(self):
+        sez = []
+        for naloga in self.naloge:
+            if naloga.resena_naloga():
+                sez.append(naloga)
+        return sez
+    #katere naloge so resene seznam
+
+class Posameznanaloga:
+    def __init__(self, ime, besedilo, resitev):
+        self.ime = ime
+        self.besedilo = besedilo
+        self.resitev = resitev
+        self.resena = False
+
+    def opravi_nalogo(self):
+        self.resena = True
+
+    def resena_naloga(self):
+        return self.resena
